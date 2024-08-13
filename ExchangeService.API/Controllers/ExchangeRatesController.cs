fef0831242df6453de037f7a9591c62a2e48fe02
Filchenko Nikita <filchenko.niki@mail.ru>
2024-08-13 17:07:49 +0300

    refactor: deleted comments

--- ExchangeService.API/Controllers/ExchangeRatesController.cs
+++ ExchangeService.API/Controllers/ExchangeRatesController.cs
@@ -30,21 +30,12 @@ namespace ExchangeService.API.Controllers
         [HttpGet("{date}/{Cur_Abbreviation}")]
         public async Task<IActionResult> GetExchangeRate(DateTime date, string currencyCode)
         {
-            // Проверяем и загружаем данные для конкретной даты и валюты
             var result = await _exchangeRateService.ValidateAndLoadExchangeRateAsync(date, currencyCode);
             if (!result)
             {
                 return NotFound();
             }
             return Ok(result);
-            // Получаем данные для конкретного кода валюты
-            /*var rate = await _exchangeRateService.GetExchangeRateByDateAndCodeAsync(date, currencyCode);
-            if (rate == null)
-            {
-                return NotFound("Exchange rate not found for the given date and currency code.");
-            }
-
-            return Ok(result);*/
         }
     }
 }
